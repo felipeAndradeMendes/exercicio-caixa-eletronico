731387402d1b1c1ae1763293f1921c5dae358343
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-10-03 14:34:22 -0300

    Initial commit

---
+++ src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
@@ -0,0 +1,44 @@
+package com.trybe.acc.java.caixaeletronico;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testes para a classe Banco")
+class BancoTest {
+
+  @Test
+  @DisplayName("20 - Testa o gerador de número único para nova conta.")
+  void gerarNumeroNovaContaTest() {
+    fail("Não implementado");
+  }
+
+  @Test
+  @DisplayName("21 - Testa o método adicionar pessoa cliente retorna o objeto pessoa cliente.")
+  void adicionarPessoaClienteTest() {
+    fail("Não implementado");
+
+  }
+
+  @Test
+  @DisplayName("22 - Testa o método login da pessoa cliente retorna o objeto pessoa cliente corretamente.")
+  void pessoaClienteLoginTest() {
+    fail("Não implementado");
+
+  }
+
+  @Test
+  @DisplayName("23 - Testa se o método transferir fundos está transferindo corretamente.")
+  void depositarTestTransferirFundosTestmostrarExtratoTest() {
+    fail("Não implementado");
+
+  }
+
+  @Test
+  @DisplayName("24 - Testa se o método sacar está funcionando corretamente.")
+  void depositarTestSacarTestMostrarExtratoTest() {
+    fail("Não implementado");
+
+  }
+
+}
---
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -0,0 +1,40 @@
+package com.trybe.acc.java.caixaeletronico;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testes dos métodos da classe Transação")
+class TransacaoTest {
+
+
+  @Test
+  @DisplayName("1 - Testa o método construtor da classe Transação.")
+  void construtorTest() {
+    fail("Não implementado");
+
+  }
+
+
+  @Test
+  @DisplayName("2 - Testa o método Getter do atributo quantia.")
+  void getQuantiaTest() {
+    fail("Não implementado");
+
+  }
+
+  @Test
+  @DisplayName("3 - Testa o método retornar resumo transação.")
+  void retornarResumoTransacaoTest() {
+    fail("Não implementado");
+
+  }
+
+  @Test
+  @DisplayName("4 - Testa o método instante está gerando o instante corretamente.")
+  void retornarInstanteTest() {
+    fail("Não implementado");
+
+  }
+
+}
